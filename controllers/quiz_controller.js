d0114df63537732126c39f09690c1b509511f3dc
hectsala <hectsala@github.com>
2015-07-08 13:51:02 -0500

    Validacion

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,15 @@
 var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye :quizId
+// Autoload :Id
 exports.load = function(req, res, next, quizId) {
   models.Quiz.find(quizId).then(
     function(quiz) {
       if (quiz) {
         req.quiz = quiz;
         next();
-      } else { next(new Error('No existe quizId=' + quizId)); }
+      } else { next(new Error('No existe quizId=' + quizId))}
     }
-  ).catch(function(error) { next(error);});
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes
@@ -19,22 +19,22 @@ exports.index = function(req, res) {
      var buscar = '%'+(req.query.search || '').replace(" ", "%")+'%';
      models.Quiz.findAll({where:["pregunta like ?", buscar],order:'pregunta ASC'}).then(
        function(quizes) {
-         res.render('quizes/index', { quizes: quizes});
+         res.render('quizes/index.ejs', { quizes: quizes, errors: []});
        }
-     ).catch(function(error) { next(error);});
+     ).catch(function(error) { next(error)});
   } else { 
 
      models.Quiz.findAll().then(
        function(quizes) {
-         res.render('quizes/index', { quizes: quizes});
+         res.render('quizes/index.ejs', { quizes: quizes, errors: []});
        }
-     ).catch(function(error) { next(error);})
+     ).catch(function(error) { next(error)});
   }
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -43,7 +43,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: [] });
 };
 
 // GET /quizes/new
@@ -52,21 +52,30 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');  
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
 };
 
 // GET /author
 exports.author = function(req, res) {
   res.render('author', { nombre: 'HmAhDyGg Soft S.A',
-  imagen: '../images/mp.jpg', video: '../images/peru.mp4' });
+  imagen: '../images/mp.jpg', video: '../images/peru.mp4', errors: [] });
 };
